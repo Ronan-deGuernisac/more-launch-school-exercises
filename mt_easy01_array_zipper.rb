661e1b3b9ebe36b8395c630f12b54e1b477f520c
Darren Burgess <darrentburgess@gmail.com>
2016-06-15 21:49:04 -0400

    add array#zip method exercise

--- mt_easy01_array_zipper.rb
+++ mt_easy01_array_zipper.rb
@@ -14,3 +14,27 @@ end
 
 new_array = zip([1, 2, 3], [4, 5, 6])
 p new_array
+
+def zip2(array1, array2)
+  result = []
+  array1.each_with_index do |element, index|
+    result << element << array2[index]
+  end
+  result
+end
+
+new_array2 = zip2([1, 2, 3], [4, 5, 6])
+p new_array2
+
+def zip3(array1, array2)
+  result = []
+  index = 0
+  while index < array1.size
+    result << array1[index] << array2[index]
+    index += 1
+  end
+  result
+end
+
+new_array3 = zip3([1, 2, 3], [4, 5, 6])
+p new_array3
