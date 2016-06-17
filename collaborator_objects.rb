9a2578d742bc98e2a10fd28fb31db1c861478eca
Darren Burgess <darrentburgess@gmail.com>
2016-06-16 20:33:14 -0400

    add collaborator objects exercise

---
+++ collaborator_objects.rb
@@ -0,0 +1,36 @@
+class Person
+  attr_accessor :name, :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+  def list_pets
+    pets.each do |pet|
+      puts pet.name
+    end
+  end
+end
+
+class Pets
+  attr_accessor :name, :breed
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+end
+
+class Dog < Pets
+end
+
+class Cat < Pets
+end
+
+bob = Person.new 'Robert'
+spike = Dog.new 'Spike', 'Bulldog'
+missy = Cat.new 'Missy', 'Domestic'
+bob.pets << spike
+bob.pets << missy 
+bob.list_pets
